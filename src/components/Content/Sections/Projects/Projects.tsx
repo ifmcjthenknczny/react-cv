cbb745675a54aabb13f5625f51e8c85e2bf3e955
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2025-01-19 00:44:53 +0100

    added links to projects

--- src/components/Content/Sections/Projects/Projects.tsx
+++ src/components/Content/Sections/Projects/Projects.tsx
@@ -13,8 +13,9 @@ type ProjectType = {
     name: string
     owner?: string
     description: string
+    link?: string
 }
 
 const ProjectContent = ({ projects }: { projects: ProjectType[] }) => <div className={styles.projects}>{projects.map((project, index) => <Project key={index} {...project} />)}</div>
 
-const Project = ({ name, owner, description }: ProjectType) => <div className={styles.project}><div className={styles.heading}>{name}{owner ? ` @${owner}` : ''}</div><div className={styles.description}>{description}</div></div>
+const Project = ({ name, link, owner, description }: ProjectType) => <div className={styles.project}><div className={styles.heading}>{link ? <a href={link}>{name}</a> : name}{owner ? ` @${owner.toLowerCase()}` : ''}</div><div className={styles.description}>{description}</div></div>
