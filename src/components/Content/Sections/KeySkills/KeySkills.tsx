98864a1d45bb0daacaef7fc29e980049e5ea3cef
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2025-02-07 17:03:57 +0100

    logos in key tech skills

--- src/components/Content/Sections/KeySkills/KeySkills.tsx
+++ src/components/Content/Sections/KeySkills/KeySkills.tsx
@@ -1,5 +1,6 @@
 import Block from '../../Block'
 import React from 'react'
+import { colord } from 'colord'
 import { determineFontColor } from '../../../../helpers/color'
 import { getData } from '../../../../helpers/data'
 import styles from './KeySkills.module.scss'
@@ -13,7 +14,7 @@ const KeySkills = () => <Block heading="Key technical Skills" content={<KeySkill
 export type SkillType = {
     name: string
     logoUrl: string
-    color?: string
+    color: string
     width?: number // 0 - 100
     fontColor?: string
 }
@@ -21,14 +22,16 @@ export type SkillType = {
 const KeySkillsContent = ({ skills }: { skills: SkillType[] }) => <div className={styles.keySkills}>{skills.map((skill, i) => <KeySkill key={i} skill={skill} index={i} />)}</div>
 
 const KeySkill = ({ skill, index }: { skill: SkillType, index: number }) => {
+    const computedBackgroundColor = skill.logoUrl ? colord(skill.color).darken(0).toHex() : skill.color
     return (
         <div className={styles.keySkill}>
             <div style={{
-                backgroundColor: skill.color,
+                backgroundColor: computedBackgroundColor,
                 opacity: OPACITY,
-                color: skill.fontColor ?? determineFontColor(skill.color ?? '#ffffff'),
+                color: skill.fontColor ?? determineFontColor(computedBackgroundColor ?? '#ffffff'),
                 width: `${calculateWidth(95, 80, index, skills.length)}%`,
-            }}>
+            }} className={styles.keySkillContainer}>
+                {skill.logoUrl && <img src={skill.logoUrl} className={styles.logo} />}
                 <span className={styles.name}>{skill.name.toLowerCase()}</span>
             </div>
         </div>
