23969a6716c099ede30009594eba5ea3d2cd48c6
ifmcjthenknczny <maciej.konieczny.1993@gmail.com>
2026-02-27 22:18:56 +0100

    split data utils

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -12,13 +12,6 @@ export const randomInt = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min)) + min
 }
 
-export const hexToRgba = (hex: string, alpha: number) => {
-    const r = parseInt(hex.slice(1, 3), 16)
-    const g = parseInt(hex.slice(3, 5), 16)
-    const b = parseInt(hex.slice(5, 7), 16)
-    return `rgba(${r}, ${g}, ${b}, ${alpha})`
-}
-
 export const removeElements = <T>(array: T[], excludeArray: T[]): T[] =>
     array.filter(
         (value, index, self) =>
